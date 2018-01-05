6f5bd954c2cf4835cebd66e1cdb0f9880115f988
Maxime Gaudreault Proulx <swcommander@gmail.com>
2018-01-04 22:37:07 -0500

    Added error handling on the client

--- src/list/index.ts
+++ src/list/index.ts
@@ -1,20 +1,28 @@
 import { autoinject } from "aurelia-dependency-injection";
 // import { Router, RouterConfiguration } from "aurelia-router";
-// import { I18N } from "aurelia-i18n";
+import { EventAggregator } from "aurelia-event-aggregator";
+import { I18N, BaseI18N } from "aurelia-i18n";
 // import { PLATFORM } from "aurelia-pal";
 import { SharedListService } from "./services/shared-list-service";
 import { UserAlertSummary } from "../app/shared/models/user-alert-summary";
 
 @autoinject()
-export class SharedListPage {
+export class SharedListPage extends BaseI18N {
     // private _i18n: I18N;
     private _sharedListService: SharedListService;
     private _listId: string;
 
     userName: string;
+    errorMessage: string;
     alerts: Array<UserAlertSummary> = [];
 
-    constructor(/*i18n: I18N, */sharedListService: SharedListService) {
+    constructor(
+            sharedListService: SharedListService,
+            i18n: I18N,
+            element: Element,
+            ea: EventAggregator) {
+        super(i18n, element, ea);
+
         // this._i18n = i18n;
         this._sharedListService = sharedListService;
     }
@@ -29,11 +37,23 @@ export class SharedListPage {
         if (route.listId) {
             this._listId = route.listId;
 
-            var list = await this._sharedListService.get(this._listId);
-            this.userName = list.userName;
-            this.alerts = list.alerts;
+            try {
+                const list = await this._sharedListService.get(this._listId);
+                if (!list) {
+                    throw new Error();
+                }
 
-            routeConfig.navModel.title = list.name;
+                this.userName = list.userName;
+                this.alerts = list.alerts;
+
+                routeConfig.navModel.title = list.name;
+            } catch(e) {
+                if (e.status === 404) {
+                    this.errorMessage = "lists.errors.notFound";
+                } else if (e.status === 401) {
+                    this.errorMessage = "lists.errors.unauthorized";
+                }
+            }
         }
     }
 }
