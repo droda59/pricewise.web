d726e44e1e33e0c08d88716dab0644969dc53a0e
Maxime Gaudreault Proulx <swcommander@gmail.com>
2018-01-23 21:45:38 -0500

    Fixed missing sources in home page

--- src/anonymous/homepage/index.ts
+++ src/anonymous/homepage/index.ts
@@ -1,2 +1,30 @@
-export class Welcome {
+import { autoinject } from "aurelia-dependency-injection";
+import { EventAggregator } from "aurelia-event-aggregator";
+import { I18N, BaseI18N } from "aurelia-i18n";
+import { AureliaConfiguration } from "aurelia-configuration";
+import { Source } from "../../shared/models/source";
+import { SourcesService } from "../../shared/services/sources-service";
+
+@autoinject()
+export class Welcome extends BaseI18N {
+    private _sourcesService: SourcesService;
+
+    sources: Array<Source>;
+    chromeAppUrl: string;
+
+    constructor(
+            sourcesService: SourcesService,
+            configure: AureliaConfiguration,
+            i18n: I18N,
+            element: Element,
+            ea: EventAggregator) {
+        super(i18n, element, ea);
+
+        this._sourcesService = sourcesService;
+        this.chromeAppUrl = configure.get("chrome");
+    }
+
+    activate(): void {
+        this.sources = this._sourcesService.sources;
+    }
 }
--- src/anonymous/shared/components/anonymous-nav-bar.ts
+++ src/anonymous/shared/components/anonymous-nav-bar.ts
@@ -3,26 +3,21 @@ import { Router } from "aurelia-router";
 import { EventAggregator } from "aurelia-event-aggregator";
 import { I18N, BaseI18N } from "aurelia-i18n";
 import { AureliaConfiguration } from "aurelia-configuration";
-import { Source } from "../../../shared/models/source";
 import { AuthenticationService } from "../../../shared/services/authentication-service";
-import { SourcesService } from "../../../shared/services/sources-service";
 import auth0 from "auth0-js";
 
 @autoinject()
 export class AnonymousNavBar extends BaseI18N {
     private _authenticationService: AuthenticationService;
-    private _sourcesService: SourcesService;
     private _router: Router;
     private _i18n: I18N;
 
     isAuthenticated: boolean;
-    sources: Array<Source>;
     chromeAppUrl: string;
 
     constructor(
             router: Router,
             authenticationService: AuthenticationService,
-            sourcesService: SourcesService,
             configure: AureliaConfiguration,
             i18n: I18N,
             element: Element,
@@ -35,14 +30,12 @@ export class AnonymousNavBar extends BaseI18N {
 
         this._router = router;
         this._authenticationService = authenticationService;
-        this._sourcesService = sourcesService;
         this._i18n = i18n;
         this.chromeAppUrl = configure.get("chrome");
     }
 
     activate(): void {
         this.isAuthenticated = this._authenticationService.isAuthenticated();
-        this.sources = this._sourcesService.sources;
 
         if (this.isAuthenticated) {
             this._router.navigateToRoute("user");
